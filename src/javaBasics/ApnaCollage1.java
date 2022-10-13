2bc5c67cb1cccd14a4172b37be4757d69818ba3e
Sagar Pardeshi <sagartesting23@gmail.com>
2022-10-14 05:29:19 +0530

    New file

---
+++ src/javaBasics/ApnaCollage1.java
@@ -0,0 +1,47 @@
+package javaBasics;
+
+import java.util.Scanner;
+
+//Arrays
+public class ApnaCollage {
+
+	// Driver program to test above function
+	public static void main (String[] args)
+	{
+		//Take input for array size
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the rows of array: ");
+		int rows = sc.nextInt();
+		System.out.print("Enter the cols of array: ");
+		int cols= sc.nextInt();
+		
+		int numbers[][] = new int[rows][cols];
+		
+		//Initialize the array
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++) {
+				numbers[i][j]=sc.nextInt();	
+			}	
+		}
+		
+		sc.close();
+		
+		int x=6;
+	
+		//Print the array
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++) {
+					System.out.print(numbers[i][j]+" ");		
+			}	
+			System.out.println();
+		}
+		//Print the value of index
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++) {
+				if(numbers[i][j]==6) {
+					System.out.print("x=6 is at index: "+i+""+j);	
+				}	
+			}	
+		}
+	}
+}
